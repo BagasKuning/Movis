2e8a2fba5d65e3eee1896baafafe52bd5766eaf5
BagasKuning <dwibagaskara55@gmail.com>
2023-12-15 10:29:39 +0700

    closed element problem

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,19 +1,31 @@
-import instagram from "./../images/instagram.png"
-import facebook from "./../images/facebook.png"
-import github from "./../images/github.png"
+import instagram from "./../images/instagram.png";
+import facebook from "./../images/facebook.png";
+import github from "./../images/github.png";
 
 export default function SideBar() {
   return (
-    <div className="fixed top-0 z-50 h-screen w-screen flex justify-end items-center">
-      <div className="bg-[#2f3335] pr-[22px] pl-3 py-5 flex gap-4 flex-col rounded-tl-xl rounded-bl-xl">
-        <a href="https://www.instagram.com/d.bagask_/?hl=id" alt="" className="w-8 h-8 rounded-md" >
-            <img src={instagram} alt="" />
+    <div className="z-50 fixed top-1/2 right-0 -translate-y-1/2 flex items-center">
+      <div className="z-50 bg-[#2f3335] sm:pr-4 pr-3 sm:pl-3 pl-2 sm:py-5 py-4 flex gap-4 flex-col rounded-tl-xl rounded-bl-xl">
+        <a
+          href="https://www.instagram.com/d.bagask_/?hl=id"
+          alt=""
+          className="w-5 h-5 sm:w-8 sm:h-8 rounded-md"
+        >
+          <img src={instagram} alt="" />
         </a>
-        <a href="https://www.facebook.com/dwi.bagaskara.9847/" alt="" className="w-8 h-8 rounded-md" >
-            <img src={facebook} alt="" />
+        <a
+          href="https://www.facebook.com/dwi.bagaskara.9847/"
+          alt=""
+          className="w-5 h-5 sm:w-8 sm:h-8 rounded-md"
+        >
+          <img src={facebook} alt="" />
         </a>
-        <a href="https://github.com/BagasKuning" alt="" className="w-8 h-8 bg-[#ffffff] rounded-md" >
-            <img src={github} alt="" />
+        <a
+          href="https://github.com/BagasKuning"
+          alt=""
+          className="w-5 h-5 sm:w-8 sm:h-8 bg-[#ffffff] rounded-md"
+        >
+          <img src={github} alt="" />
         </a>
       </div>
     </div>
